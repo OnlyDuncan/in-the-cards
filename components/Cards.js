be852f69310c6ee2bc4cafd715720fab5ed348ed
Duncan Payne <Duncan.C.Payne@gmail.com>
2023-10-12 12:07:11 -0500

    Basic Architecture Completed

---
+++ components/Cards.js
@@ -0,0 +1,12 @@
+import Card from './Card';
+
+export default function Cards() {
+
+    return (
+        <div>
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
