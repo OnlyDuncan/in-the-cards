212ccb392b7ceb43203d3cf09bca0e9053e65f5c
Duncan Payne <Duncan.C.Payne@gmail.com>
2023-10-16 15:14:53 -0500

    Deck Shuffle And Data Aquisition From Firebase Backend Complete

--- components/Cards.js
+++ components/Cards.js
@@ -1,48 +1,7 @@
 import Card from './Card';
 import Image from 'next/image';
 import { db } from '../utils/firebase';
-import { firebase, firestore, doc, getDoc, getDocs, getDocFromCache, addDoc, collection, subcollection, query, where } from 'firebase/firestore';
-
-function dataConverter (ref) {
-    class Card {
-        constructor (Id, Title, Reversed, Image, Meaning) {
-            this.Id = Id;
-            this.Title = Title;
-            this.Reversed = Reversed;
-            this.Image = Image;
-            this.Meaning = Meaning;
-        }
-        toString() {
-            return this.Id + ', ' + this.Title + ', ' + this.Reversed + ', ' + this.Image + ', ' + this.Meaning;
-        }
-    }
-
-    const cardConverter = {
-        toFirestore: (card) => {
-            return {
-                Id: card.Id,
-                Title: card.Title,
-                Reversed: card.Reversed,
-                Image: card.Image,
-                Meaning: card.Meaning
-            };
-        },
-        fromFirestore: (snapshot, options) => {
-            const data = snapshot.data(options);
-            return new Card(data.Id, data.Title, data.Reversed, data.Image, data.Meaning);
-        }
-    }
-
-    const cardRef = ref.withConverter(cardConverter);
-    const docSnap = getDoc(cardRef);
-    if (docSnap.exists) {
-        const cardData = docSnap.data();
-        console.log(cardData.toString());
-        return cardData.toString();
-    } else {
-        console.log("No such document!");
-    }
-}
+import { doc, getDoc } from 'firebase/firestore';
 
 function shuffle(array) {
     let currentIndex = array.length, randomIndex;
@@ -58,69 +17,352 @@ function shuffle(array) {
     return array;
 }
 
-function idGenerator() {
-    let min = 1;
-    let max = 2;
-    let random = Math.random() * (max - min) + min;
-    return Math.floor(random);
-}
-
 async function cardPicker() {
 
-    // const cardRef = doc(db, "cards", "The Magician", "The Magician", "The Magician (Upright)");
-    // // const cardSnap = await getDoc(cardRef);
+    let tempArray = [];
+    let cardsArray = [];
+    // let spreadArray = [];
 
-    // try {
-    //     const card = await getDocFromCache(cardRef);
-    //     console.log("Cached document data:", card.data());
-    // } catch (e) {
-    //     console.log("Error getting cached document:", e);
-    // }
+    // Fool and Rundown of Process
 
-    const querySnapshot = await getDocs(collection(db, "cards", "The Magician", "The Magician"));
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-    });
+    // Get data for upright version of card and push into temp array
+    let FoolRefUpright =  doc(db, "cards", "The Fool", "The Fool", "The Fool (Upright)");
+    let FoolSnapUpright = await getDoc(FoolRefUpright);
+    const FoolUpright = FoolSnapUpright.data();
+    tempArray.push(FoolUpright);
 
-    // try {
-    //     const docRef = await addDoc(collection(db, "users"), {
-    //         first: "Ada",
-    //         last: "Lovelace",
-    //         born: 1815
-    //     });
-    //     console.log("Document written with ID: ", docRef.id);
-    // } catch (e) {
-    //     console.error("Error adding document: ", e);
-    // }
+    // Get data for reversed version of card and push into temp array
+    let FoolRefReversed =  doc(db, "cards", "The Fool", "The Fool", "The Fool (Reversed)");
+    let FoolSnapReversed = await getDoc(FoolRefReversed);
+    const FoolReversed = FoolSnapReversed.data();
+    tempArray.push(FoolReversed);
 
-    // if (cardSnap.exists()) {
-    //     console.log("Document data:", cardSnap.data()['Title']);
-    // } else {
-    //     console.log("No such document!");
-    // }
+    // Shuffle temp array
+    tempArray = shuffle(tempArray);
+    
+    // Put first index of the shuffled array into the deck array
+    cardsArray.push(tempArray[1]);
 
-    // let tempArray = [];
-    // let cardsArray = [];
+    // Erase temp array for reuse for the next cards
+    tempArray = [];
 
-    // let magicianRefUpright =  doc(db, "cards", "The Magician", "The Magician", "The Magician (Upright)");
-    // let magicianSnapUpright = await getDoc(magicianRefUpright);
-    // console.log(magicianSnapUpright.data());
+    // Magician
 
-    // const MagicianCardUpright = dataConverter(magicianRefUpright);
-    // tempArray.push(MagicianCardUpright);
-    // let magicianRefReversed = doc(db, "cards", "The Magician", "The Magician", "The Magician (Reversed)");
-    // const MagicianCardReversed = dataConverter(magicianRefReversed);
-    // tempArray.push(MagicianCardReversed);
-    // console.log(tempArray);
-    // tempArray = shuffle(tempArray);
-    // cardsArray.push(tempArray[1]);
-    // tempArray = [];
+    let magicianRefUpright =  doc(db, "cards", "The Magician", "The Magician", "The Magician (Upright)");
+    let magicianSnapUpright = await getDoc(magicianRefUpright);
+    const magicianUpright = magicianSnapUpright.data();
+    tempArray.push(magicianUpright);
+    let magicianRefReversed =  doc(db, "cards", "The Magician", "The Magician", "The Magician (Reversed)");
+    let magicianSnapReversed = await getDoc(magicianRefReversed);
+    const magicianReversed = magicianSnapReversed.data();
+    tempArray.push(magicianReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The High Priestess
+
+    let priestessRefUpright =  doc(db, "cards", "The High Priestess", "The High Priestess", "The High Priestess (Upright)");
+    let priestessSnapUpright = await getDoc(priestessRefUpright);
+    const priestessUpright = priestessSnapUpright.data();
+    tempArray.push(priestessUpright);
+    let priestessRefReversed =  doc(db, "cards", "The High Priestess", "The High Priestess", "The High Priestess (Reversed)");
+    let priestessSnapReversed = await getDoc(priestessRefReversed);
+    const priestessReversed = priestessSnapReversed.data();
+    tempArray.push(priestessReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Empress
+
+    let empressRefUpright =  doc(db, "cards", "The Empress", "The Empress", "The Empress (Upright)");
+    let empressSnapUpright = await getDoc(empressRefUpright);
+    const empressUpright = empressSnapUpright.data();
+    tempArray.push(empressUpright);
+    let empressRefReversed =  doc(db, "cards", "The Empress", "The Empress", "The Empress (Reversed)");
+    let empressSnapReversed = await getDoc(empressRefReversed);
+    const empressReversed = empressSnapReversed.data();
+    tempArray.push(empressReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Emperor
+
+    let emperorRefUpright =  doc(db, "cards", "The Emperor", "The Emperor", "The Emperor (Upright)");
+    let emperorSnapUpright = await getDoc(emperorRefUpright);
+    const emperorUpright = emperorSnapUpright.data();
+    tempArray.push(emperorUpright);
+    let emperorRefReversed =  doc(db, "cards", "The Emperor", "The Emperor", "The Emperor (Reversed)");
+    let emperorSnapReversed = await getDoc(emperorRefReversed);
+    const emperorReversed = emperorSnapReversed.data();
+    tempArray.push(emperorReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Hierophant
+
+    let hierophantRefUpright =  doc(db, "cards", "The Hierophant", "The Hierophant", "The Hierophant (Upright)");
+    let hierophantSnapUpright = await getDoc(hierophantRefUpright);
+    const hierophantUpright = hierophantSnapUpright.data();
+    tempArray.push(hierophantUpright);
+    let hierophantRefReversed =  doc(db, "cards", "The Hierophant", "The Hierophant", "The Hierophant (Reversed)");
+    let hierophantSnapReversed = await getDoc(hierophantRefReversed);
+    const hierophantReversed = hierophantSnapReversed.data();
+    tempArray.push(hierophantReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Lovers
+
+    let loversRefUpright =  doc(db, "cards", "The Lovers", "The Lovers", "The Lovers (Upright)");
+    let loversSnapUpright = await getDoc(loversRefUpright);
+    const loversUpright = loversSnapUpright.data();
+    tempArray.push(loversUpright);
+    let loversRefReversed =  doc(db, "cards", "The Lovers", "The Lovers", "The Lovers (Reversed)");
+    let loversSnapReversed = await getDoc(loversRefReversed);
+    const loversReversed = loversSnapReversed.data();
+    tempArray.push(loversReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Chariot
+
+    let chariotRefUpright =  doc(db, "cards", "The Chariot", "The Chariot", "The Chariot (Upright)");
+    let chariotSnapUpright = await getDoc(chariotRefUpright);
+    const chariotUpright = chariotSnapUpright.data();
+    tempArray.push(chariotUpright);
+    let chariotRefReversed =  doc(db, "cards", "The Chariot", "The Chariot", "The Chariot (Reversed)");
+    let chariotSnapReversed = await getDoc(chariotRefReversed);
+    const chariotReversed = chariotSnapReversed.data();
+    tempArray.push(chariotReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // Strength
+
+    let strengthRefUpright =  doc(db, "cards", "Strength", "Strength", "Strength (Upright)");
+    let strengthSnapUpright = await getDoc(strengthRefUpright);
+    const strengthUpright = strengthSnapUpright.data();
+    tempArray.push(strengthUpright);
+    let strengthRefReversed =  doc(db, "cards", "Strength", "Strength", "Strength (Reversed)");
+    let strengthSnapReversed = await getDoc(strengthRefReversed);
+    const strengthReversed = strengthSnapReversed.data();
+    tempArray.push(strengthReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Hermit
+
+    let hermitRefUpright =  doc(db, "cards", "The Hermit", "The Hermit", "The Hermit (Upright)");
+    let hermitSnapUpright = await getDoc(hermitRefUpright);
+    const hermitUpright = hermitSnapUpright.data();
+    tempArray.push(hermitUpright);
+    let hermitRefReversed =  doc(db, "cards", "The Hermit", "The Hermit", "The Hermit (Reversed)");
+    let hermitSnapReversed = await getDoc(hermitRefReversed);
+    const hermitReversed = hermitSnapReversed.data();
+    tempArray.push(hermitReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // Wheel of Fortune
+
+    let wheelRefUpright =  doc(db, "cards", "Wheel of Fortune", "Wheel of Fortune", "Wheel of Fortune (Upright)");
+    let wheelSnapUpright = await getDoc(wheelRefUpright);
+    const wheelUpright = wheelSnapUpright.data();
+    tempArray.push(wheelUpright);
+    let wheelRefReversed =  doc(db, "cards", "Wheel of Fortune", "Wheel of Fortune", "Wheel of Fortune (Reversed)");
+    let wheelSnapReversed = await getDoc(wheelRefReversed);
+    const wheelReversed = wheelSnapReversed.data();
+    tempArray.push(wheelReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // Justice
+
+    let justiceRefUpright =  doc(db, "cards", "Justice", "Justice", "Justice (Upright)");
+    let justiceSnapUpright = await getDoc(justiceRefUpright);
+    const justiceUpright = justiceSnapUpright.data();
+    tempArray.push(justiceUpright);
+    let justiceRefReversed =  doc(db, "cards", "Justice", "Justice", "Justice (Reversed)");
+    let justiceSnapReversed = await getDoc(justiceRefReversed);
+    const justiceReversed = justiceSnapReversed.data();
+    tempArray.push(justiceReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Hanged Man
+
+    let hangedRefUpright =  doc(db, "cards", "The Hanged Man", "The Hanged Man", "The Hanged Man (Upright)");
+    let hangedSnapUpright = await getDoc(hangedRefUpright);
+    const hangedUpright = hangedSnapUpright.data();
+    tempArray.push(hangedUpright);
+    let hangedRefReversed =  doc(db, "cards", "The Hanged Man", "The Hanged Man", "The Hanged Man (Reversed)");
+    let hangedSnapReversed = await getDoc(hangedRefReversed);
+    const hangedReversed = hangedSnapReversed.data();
+    tempArray.push(hangedReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // Death
+
+    let deathRefUpright =  doc(db, "cards", "Death", "Death", "Death (Upright)");
+    let deathSnapUpright = await getDoc(deathRefUpright);
+    const deathUpright = deathSnapUpright.data();
+    tempArray.push(deathUpright);
+    let deathRefReversed =  doc(db, "cards", "Death", "Death", "Death (Reversed)");
+    let deathSnapReversed = await getDoc(deathRefReversed);
+    const deathReversed = deathSnapReversed.data();
+    tempArray.push(deathReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // Temperance
+
+    let temperanceRefUpright =  doc(db, "cards", "Temperance", "Temperance", "Temperance (Upright)");
+    let temperanceSnapUpright = await getDoc(temperanceRefUpright);
+    const temperanceUpright = temperanceSnapUpright.data();
+    tempArray.push(temperanceUpright);
+    let temperanceRefReversed =  doc(db, "cards", "Temperance", "Temperance", "Temperance (Reversed)");
+    let temperanceSnapReversed = await getDoc(temperanceRefReversed);
+    const temperanceReversed = temperanceSnapReversed.data();
+    tempArray.push(temperanceReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Devil
+
+    let devilRefUpright =  doc(db, "cards", "The Devil", "The Devil", "The Devil (Upright)");
+    let devilSnapUpright = await getDoc(devilRefUpright);
+    const devilUpright = devilSnapUpright.data();
+    tempArray.push(devilUpright);
+    let devilRefReversed =  doc(db, "cards", "The Devil", "The Devil", "The Devil (Reversed)");
+    let devilSnapReversed = await getDoc(devilRefReversed);
+    const devilReversed = devilSnapReversed.data();
+    tempArray.push(devilReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Tower
+
+    let towerRefUpright =  doc(db, "cards", "The Tower", "The Tower", "The Tower (Upright)");
+    let towerSnapUpright = await getDoc(towerRefUpright);
+    const towerUpright = towerSnapUpright.data();
+    tempArray.push(towerUpright);
+    let towerRefReversed =  doc(db, "cards", "The Tower", "The Tower", "The Tower (Reversed)");
+    let towerSnapReversed = await getDoc(towerRefReversed);
+    const towerReversed = towerSnapReversed.data();
+    tempArray.push(towerReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Star
+
+    let starRefUpright =  doc(db, "cards", "The Star", "The Star", "The Star (Upright)");
+    let starSnapUpright = await getDoc(starRefUpright);
+    const starUpright = starSnapUpright.data();
+    tempArray.push(starUpright);
+    let starRefReversed =  doc(db, "cards", "The Star", "The Star", "The Star (Reversed)");
+    let starSnapReversed = await getDoc(starRefReversed);
+    const starReversed = starSnapReversed.data();
+    tempArray.push(starReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Moon
+
+    let moonRefUpright =  doc(db, "cards", "The Moon", "The Moon", "The Moon (Upright)");
+    let moonSnapUpright = await getDoc(moonRefUpright);
+    const moonUpright = moonSnapUpright.data();
+    tempArray.push(moonUpright);
+    let moonRefReversed =  doc(db, "cards", "The Moon", "The Moon", "The Moon (Reversed)");
+    let moonSnapReversed = await getDoc(moonRefReversed);
+    const moonReversed = moonSnapReversed.data();
+    tempArray.push(moonReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The Sun
+
+    let sunRefUpright =  doc(db, "cards", "The Sun", "The Sun", "The Sun (Upright)");
+    let sunSnapUpright = await getDoc(sunRefUpright);
+    const sunUpright = sunSnapUpright.data();
+    tempArray.push(sunUpright);
+    let sunRefReversed =  doc(db, "cards", "The Sun", "The Sun", "The Sun (Reversed)");
+    let sunSnapReversed = await getDoc(sunRefReversed);
+    const sunReversed = sunSnapReversed.data();
+    tempArray.push(sunReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // Judgement
+
+    let judgementRefUpright =  doc(db, "cards", "Judgement", "Judgement", "Judgement (Upright)");
+    let judgementSnapUpright = await getDoc(judgementRefUpright);
+    const judgementUpright = judgementSnapUpright.data();
+    tempArray.push(judgementUpright);
+    let judgementRefReversed =  doc(db, "cards", "Judgement", "Judgement", "Judgement (Reversed)");
+    let judgementSnapReversed = await getDoc(judgementRefReversed);
+    const judgementReversed = judgementSnapReversed.data();
+    tempArray.push(judgementReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // The World
+
+    let worldRefUpright =  doc(db, "cards", "The World", "The World", "The World (Upright)");
+    let worldSnapUpright = await getDoc(worldRefUpright);
+    const worldUpright = worldSnapUpright.data();
+    tempArray.push(worldUpright);
+    let worldRefReversed =  doc(db, "cards", "The World", "The World", "The World (Reversed)");
+    let worldSnapReversed = await getDoc(worldRefReversed);
+    const worldReversed = worldSnapReversed.data();
+    tempArray.push(worldReversed);
+    tempArray = shuffle(tempArray);
+    cardsArray.push(tempArray[1]);
+    tempArray = [];
+
+    // Final Steps
+
+    // Shuffle deck array
+    cardsArray = shuffle(cardsArray);
+
+    // Make variables for the first three cards in the deck and return them in an array
+    let cardOne = cardsArray[1];
+    let cardTwo = cardsArray[2];
+    let cardThree = cardsArray[3];
+
+    return [cardOne, cardTwo, cardThree];
 
 }
 
-export default function Cards() {
+export default async function Cards() {
 
-    cardPicker();
+    let spread = await cardPicker();
+
+    console.log(spread);
+
+    let cardOne = spread[1];
+    let cardTwo = spread[2];
+    let cardThree = spread[3];
 
     return (
         <div>
--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = {
+const nextConfig = {
+    reactStrictMode: true,
     images: {
         domains: ['www.tarotcardmeanings.net']
+    },
+    env: {
+        NEXT_PUBLIC_PROJECT_ID: 'tarot-reading-app-42e6e'
     }
 }
+
+module.exports = nextConfig;
