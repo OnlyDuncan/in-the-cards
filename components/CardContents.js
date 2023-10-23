d4e50c7439239b5149700fbb8f729cd8c30f22d5
Duncan Payne <Duncan.C.Payne@gmail.com>
2023-10-23 15:18:47 -0500

    Testing Modals

--- components/CardContents.js
+++ components/CardContents.js
@@ -1,21 +1,84 @@
 import CardBackImage from '../public/Images/CardBack.webp';
+import Modal from 'react-modal';
 import Image from 'next/image';
 import { useState } from 'react';
 
 export default function CardContents ({onClick, ...props}) {
 
+    const tarotCard = props;
+    const tarotTitle = `${tarotCard.Title}`;
+    const tarotImage = `${tarotCard.Image}`;
+    const tarotMeaning = `${tarotCard.Meaning}`;
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const styles = {
+        overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.4)',
+            overflowY: 'scroll'
+        },
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#404355',
+            width: '40rem',
+            maxWidth: '90%',
+            maxHeight: '95vh',
+            position: 'relative'
+        }
+    }
+
     return (
         <div className="card" onClick={onClick}>
             <div className="card-back">
                 <Image
-                    src={props.Image}
+                    src={tarotImage}
                     width="0"
                     height="0"
                     sizes="100vh"
                     className="h-full w-auto"
                     alt="Tarot Card"
+                    onClick={() => setIsOpen(true)}
                 />
             </div>
+            <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={styles} className="card-back">
+                <div className="flex-col justify-center">
+                    <div className="flex">
+                        <button onClick={() => setIsOpen(false)} className="mr-0 ml-auto p-3">
+                            <Image
+                                src="/Images/Close.svg"
+                                width={30}
+                                height={30}
+                            />
+                        </button>
+                    </div>
+                    <br />
+                    <h1
+                        className="text-4xl text-center justify-center text-white"
+                    >
+                        {tarotTitle}
+                    </h1>
+                    <br />
+                    <div className="flex justify-center">
+                        <Image
+                            src={tarotImage}
+                            width="0"
+                            height="0"
+                            sizes="100vh"
+                            className="h-60 w-auto"
+                            alt={tarotTitle}
+                        />
+                    </div>
+                    <br />
+                    <p className="flex text-center text-white pb-10 px-10">
+                        {tarotMeaning}
+                    </p>
+                </div>
+            </Modal>
             <div className="card-front">
                 <Image
                     src={CardBackImage}
--- components/Cards.js
+++ components/Cards.js
@@ -1,6 +1,5 @@
 "use client";
 
-import ReadMoreModal from './Modal.js';
 import CardContents from './CardContents';
 import { CSSTransition } from 'react-transition-group';
 import { useState } from 'react';
@@ -71,21 +70,12 @@ export default function Cards ({...props}) {
                                 classNames='flip'
                             >
                                 <CardContents {...spread[0]} onClick={() => {
-                                    setShowFrontPast((v) => !v);
+                                    setShowFrontPast(false);
                                 }} />
                             </CSSTransition>
                             <div className="flex justify-center text-align align-center ml-2 mt-2">
                                 The Past
                             </div>
-                            <CSSTransition
-                                in={showFrontPast}
-                                timeout={300}
-                                classNames='flip mt-4'
-                            >
-                                <ReadMoreModal {...spread[0]} onClick={() => {
-                                    setShowFrontPast((v) => !v);
-                                }} />
-                            </CSSTransition>
                         </div>
                     </div>
                 </div>
@@ -98,13 +88,13 @@ export default function Cards ({...props}) {
                                 classNames='flip'
                             >
                                 <CardContents {...spread[1]} onClick={() => {
-                                    setShowFrontPresent((v) => !v);
+                                    setShowFrontPresent(false);
                                 }} />
                             </CSSTransition>
+                            <div className="flex justify-center text-align align-center ml-2 mr-2 mt-2">
+                                The Present
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex justify-center text-align align-center ml-2 mr-2 mt-2">
-                        The Present
                     </div>
                 </div>
                 <div className="flex flex-col justify-center">
@@ -116,13 +106,13 @@ export default function Cards ({...props}) {
                                 classNames='flip'
                             >
                                 <CardContents {...spread[2]} onClick={() => {
-                                    setShowFrontFuture((v) => !v);
+                                    setShowFrontFuture(false);
                                 }} />
                             </CSSTransition>
+                            <div className="flex justify-center text-align align-center mr-2 mt-2">
+                                The Future
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex justify-center text-align align-center mr-2 mt-2">
-                        The Future
                     </div>
                 </div>
             </div>
